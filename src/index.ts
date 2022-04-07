6d4394a824d7d8a9dad195fc0a45fe0750d9edac
Hiromi Takahashi <hiromix222@gmail.com>
2022-04-07 22:54:47 +0900

    pattern 追加

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,7 @@ function hello(name: string): string {
     console.log("Hello, " + x.name);
   }
   //PointlikeインターフェースとNamedインターフェースの両方の特性を持つ形で定義
+  //構造的型システム（オブジェクトの構造が同じであれば型に互換性があるということ）
   const obj = {
       x:0,
       y:100,
@@ -26,3 +27,23 @@ function hello(name: string): string {
   };
   logPoint(obj);
   logName(obj);
+
+  let num = 123;
+  //以下とすると、エラー   src/index.ts:31:3 - error TS2322: Type 'string' is not assignable to type 'number'.
+  //num = "aaa";
+  let num2:number =12;
+  //以下も型相違でエラー
+  //let num3:number="1";
+
+  //クラスの書き方
+  class Person {
+      name:string;
+      age:number;
+      constructor(name:string,age:number) {
+          this.name=name;
+          this.age=age;
+      }
+  }
+  // new で生成する
+  const p1 = new Person("hogehoge",1);
+  logName(p1);
