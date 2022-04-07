af3d9600b52f0fe9da0b94c090378f63a075b6ab
Hiromi Takahashi <hiromix222@gmail.com>
2022-04-07 22:39:56 +0900

    modify src

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,26 @@ function hello(name: string): string {
   }
   
   console.log(hello("TypeScript"));
+
+  interface Pointlike {
+      x:number;
+      y:number;
+  }
+
+  interface Named {
+      name:String;
+  }
+  function logPoint (point:Pointlike) {
+      console.log("X = "+point.x+",Y="+point.y);
+  }
+  function logName(x: Named) {
+    console.log("Hello, " + x.name);
+  }
+  //PointlikeインターフェースとNamedインターフェースの両方の特性を持つ形で定義
+  const obj = {
+      x:0,
+      y:100,
+      name:"Origin",
+  };
+  logPoint(obj);
+  logName(obj);
