98779f7bb96c40c9533702f2aea63d4bb62c078f
Hiromi Takahashi <hiromix222@gmail.com>
2022-04-08 23:51:04 +0900

    add Enum

--- src/index.ts
+++ src/index.ts
@@ -47,3 +47,14 @@ function hello(name: string): string {
   // new で生成する
   const p1 = new Person("hogehoge",1);
   logName(p1);
+
+
+  enum SEASONS{SPRING, SUMMER, AUTUMN, WINTER};
+  console.log(SEASONS.AUTUMN);
+let season:SEASONS;         
+season = SEASONS.SUMMER;    
+if(season == SEASONS.SUMMER){
+  console.log("夏ですね");
+} else {
+  console.log("夏ではないですね");
+}
